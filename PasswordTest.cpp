2b2d228510247c7d2df14c7a0cd78dcd39dd400a
emmaisaway <mtillman@csuchico.edu>
2025-11-05 11:48:52 -0800

    add test cases

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -2,6 +2,7 @@
  * Unit Tests for Password class
 **/
 
+#include <cassert>
 #include <gtest/gtest.h>
 #include "Password.h"
 
@@ -19,4 +20,40 @@ TEST(PasswordTest, single_letter_password)
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
+}
+TEST(PasswordTest, zero_letter_password)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("");
+    ASSERT_EQ(0, actual);
+}
+TEST(PasswordTest, repeated_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("aaa");
+    ASSERT_EQ(3, actual);
+}
+TEST(PasswordTest, non_alphanum_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("{{aaa");
+    ASSERT_EQ(2, actual);
+}
+TEST(PasswordTest, empty_string_casechange)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("");
+    ASSERT_EQ(false, actual);
+}
+TEST(PasswordTest, mixed_case)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("aA");
+    ASSERT_EQ(true, actual);
+}
+TEST(PasswordTest, same_case)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("aa");
+    ASSERT_EQ(true, actual);
 }
